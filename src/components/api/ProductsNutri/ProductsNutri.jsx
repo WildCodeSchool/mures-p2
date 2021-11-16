88d64d5c0adf0fa275dec7c02dc24f99d0d2e8bf
Paul-Verdure <paul.verdure@gmail.com>
2021-11-16 17:16:36 +0100

    liens RS, retrait bouter rescan

--- src/components/api/ProductsNutri/ProductsNutri.jsx
+++ src/components/api/ProductsNutri/ProductsNutri.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./ProductsNutri.css";
 import ProductsNutriBack from './ProductsNutriBack';
 import ProductsNutriFront from './ProductsNutriFront';
