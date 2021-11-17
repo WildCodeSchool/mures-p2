e650fef71718797ee24d4bbb6af429f940c3be3e
Ranto Rabarisoa <ranto.rabarisoa@gmail.com>
2021-11-17 19:23:08 +0300

    contact page

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -13,37 +13,37 @@ import lucille from "../../assets/lucille.jpeg";
 function Contacts() {
     return (
         
-        <main>
-            <p className="who">Qui sommes nous?</p>
+        <main className="allPageContact">
+            <p className="whoWeAre">Qui sommes nous?</p>
             <div className="doorContainer">
                 <div className="wrapper">
-                    <img src={unnamed} alt="profile"/>
+                    <img className="imgContact" src={unnamed} alt="profile"/>
                     <div id="left-door" className="door"></div>
                     <div id="right-door" class="door"></div> 
                 </div>
                 <div className="wrapper">
-                    <img src={paul} alt="profile"/>
+                    <img className="imgContact" src={paul} alt="profile"/>
                     <div id="left-door" className="door"></div>
                     <div id="right-door" class="door"></div> 
                 </div>        
                 <div className="wrapper">
-                    <img src={sandra} alt="profile"/>
+                    <img className="imgContact" src={sandra} alt="profile"/>
                     <div id="left-door" className="door"></div>
                     <div id="right-door" class="door"></div> 
                 </div>        
                 <div className="wrapper">
-                    <img src={esteban} alt="profile"/>
+                    <img className="imgContact" src={esteban} alt="profile"/>
                     <div id="left-door" className="door"></div>
                     <div id="right-door" class="door"></div> 
                 </div>        
                 <div className="wrapper">
-                    <img src={lucille} alt="profile"/>
+                    <img className="imgContact" src={lucille} alt="profile"/>
                     <div id="left-door" className="door"></div>
                     <div id="right-door" class="door"></div> 
                 </div>
            </div>
             <div>
-                <p className="sainement">Consommez sainement</p>
+                <p className="healthier">Consommez sainement</p>
             </div>
            <div className="contactsHooksStyle">
                 <ContactsHook/>
--- src/components/contact/ContactsHook.jsx
+++ src/components/contact/ContactsHook.jsx
@@ -11,12 +11,12 @@ function ContactsHook() {
         console.log(data);
     }
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input  placeholder="e mail" {...register("email", {required: true})}/>
+        <form className="contactContainer" onSubmit={handleSubmit(onSubmit)}>
+            <input className="emailContact" placeholder="e mail" {...register("email", {required: true})}/>
             {errors.email && <p className="p2">Email is required</p>}
-            <textarea rows="8" cols="100" placeholder=" Message..." {...register("message", { required: true}) } />
+            <textarea className="textareaContact" rows="8" cols="100" placeholder=" Message..." {...register("message", { required: true}) } />
             {errors.message && <p className="p1">This field is required</p>}
-            <input  type="submit"/>
+            <input className="submitContact" type="submit"/>
         </form >
       
     )
