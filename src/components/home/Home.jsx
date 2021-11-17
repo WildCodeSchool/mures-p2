27ce913d3b553664352c6e9ec96507edbddc41a2
SandraMey <sandra.meyniel@gmail.com>
2021-11-17 14:39:06 +0100

    modif page home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,15 +17,6 @@ return (
             <div className="wrapbutton">   
                 <Link to="/ProductScan"><button className="buttonscan">Je scanne</button></Link>
             </div>
-
-            <article className="hometext">
-                <h1>Faites les bons choix pour votre santé</h1>
-                <p><strong>Cheesecake évalue la qualité</strong> de vos produits alimentaires en un clin d'oeil. 
-                    Utilisez <strong>le scan </strong>ou l'outil de <strong>recherche </strong>ainsi vous pourrez <strong>visualiser les 
-                    produits qui sont bons </strong>et ceux qu'il faut mieux éviter.<br/>
-                    Obtenez des<strong> recommandations</strong> de meilleurs produits si celui ci n'est pas bien classés.
-                </p>
-            </article>
         </section>
     </main>
 )
